7baf1577deeda01381bbb394700c5bc12af5df05
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2024-01-01 04:09:19 +0800

    Update main.js

--- lester/main.js
+++ lester/main.js
@@ -1,15 +1,9 @@
-var yesBtn = document.getElementById("yes")
 var noBtn = document.getElementById("no")
-var bgText = document.getElementsByClassName("small")
 
-function pressNo() {
+function pressYes() {
   noBtn.remove()
 }
 
-function pressYes() {
+function pressNo() {
   document.getElementById("bgText").style.animationName = "fade"
-}
-
-function rand(x) {
-  Math.floor(Math.random()*x)
 }
