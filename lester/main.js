e6905ac692d65c1f50cbf6399e773246a3caa308
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2024-02-28 19:49:20 +0800

    Delete lester directory

--- lester/main.js
+++
@@ -1,9 +0,0 @@
-var yesBtn = document.getElementById("yes")
-
-function pressYes() {
-  yesBtn.remove()
-}
-
-function pressNo() {
-  document.getElementById("bgText").style.animationName = "fade"
-}
